101f79d12a3e067f09a2d717c973519d3856fcc0
devevandro <dev.evandro@gmail.com>
2024-10-21 23:31:24 -0300

    feat: create route to heath check

--- server.js
+++ server.js
@@ -4,13 +4,18 @@ const app = require("express")();
 const cron = require("node-cron");
 require('dotenv').config();
 
-cron.schedule("*/5 * * * *", async () => {
+// cron.schedule("*/5 * * * *", async () => {
   try {
    const response = await axios.get(process.env.URL);
     pino.info(response.data);
   } catch (error) {
     pino.error(error);
   }
+// });
+
+app.get('/health-check', (req, res) => {
+  pino.info("Running!");
+  return res.json({ message: 'API OK!' });
 });
 
 app.listen(3000, () => {
