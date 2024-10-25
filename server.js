d54478ac7835cd7f6c7a5a6d121b2949d7bb0bef
devevandro <dev.evandro@gmail.com>
2024-10-24 21:43:13 -0300

    feat: remove cron

--- server.js
+++ server.js
@@ -4,14 +4,14 @@ const app = require("express")();
 const cron = require("node-cron");
 require('dotenv').config();
 
-cron.schedule("*/5 * * * *", async () => {
-  try {
-   const response = await axios.get(process.env.URL);
-    pino.info(response.data);
-  } catch (error) {
-    pino.error(error);
-  }
-});
+// cron.schedule("*/5 * * * *", async () => {
+//   try {
+//    const response = await axios.get(process.env.URL);
+//     pino.info(response.data);
+//   } catch (error) {
+//     pino.error(error);
+//   }
+// });
 
 app.get('/health-check', (req, res) => {
   pino.info("Running!");
